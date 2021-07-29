fe78db0e8b678be746ff18e4e038eceabf643ef9
bisratawoke <bisratawokegithub>
2021-07-29 17:21:38 +0300

    getting ready

--- src/controllers/php.sh
+++ src/controllers/php.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 
-echo "TODO: php deployment"
+
+touch Dockerfile
+
+
+echo "FROM php:apache \n COPY ./files /var/www/html " > Dockerfile
+
+
+docker build -t $domain_name .
+
+	  
+docker run --name $domain_name -d --network haproxytest_tester $domain_name
+ 
+ 
